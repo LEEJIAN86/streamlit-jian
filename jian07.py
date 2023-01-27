95d5ae8c17d8acb115b294acebf8ae3982e9fb18
LEEJIAN86 <123149534+LEEJIAN86@users.noreply.github.com>
2023-01-27 17:04:56 +0900

    Update jian07.py

--- jian07.py
+++ jian07.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 st.title('Jian\'s :orange[library] :books:
-           Ch.1_')
+           Ch.1')
 
 def  plotting_demo():
     uploaded_file = st.file_uploader("Choose a file")
