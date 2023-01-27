bd05f3b5f3b37a865d63bc293ac4d0df6f43b49c
LEEJIAN86 <123149534+LEEJIAN86@users.noreply.github.com>
2023-01-27 17:07:12 +0900

    Update jian07.py

--- jian07.py
+++ jian07.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-st.title('Jian\'s :orange[library] :books: Ch\.1\_money data')
+st.title('Jian\'s :orange[library] :books: Ch\.1\_Money Data')
 
 def  plotting_demo():
     uploaded_file = st.file_uploader("Choose a file")
