447200ec025e92080dfb7224a4bc9132772cf181
LEEJIAN86 <123149534+LEEJIAN86@users.noreply.github.com>
2023-02-01 19:13:30 +0900

    Update jian07.py

--- jian07.py
+++ jian07.py
@@ -79,7 +79,7 @@ def bar_chart():
     fig, ax = plt.subplots(figsize=(12,8))
 
     #colors = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7' ,'C8', 'C9', 'C10' ]
-    plt.bar(  x,  y,  color= 'grey' ) 
+    plt.bar(  x,  y,  color= 'lightslategray' ) 
 
     for   num ,   v    in   enumerate( y ):
         plt.text (  num -0.25  ,   v + 0.01 ,  v   )
