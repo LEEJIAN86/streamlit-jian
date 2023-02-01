d1b222be675d9dca3b2d28a7acecd6d0540bb0eb
LEEJIAN86 <123149534+LEEJIAN86@users.noreply.github.com>
2023-02-01 17:39:14 +0900

    Update jian07.py

--- jian07.py
+++ jian07.py
@@ -95,13 +95,13 @@ st.title('Jian\'s :orange[library] :books: Data Analysis')
 with st.form(key ='Form1'):
     with st.sidebar:
         
-        select_language = st.sidebar.radio('데이터 분석 결과', ('금리와 집값 빠르게 파악하기', '야구 순위와 승률 빠르게 파악하기', '다른 데이터 분석'))
+        select_language = st.sidebar.radio('데이터 분석 결과', ('금리와 집값의 상관관계', '야구 순위와 승률의 상관관계', '다른 데이터 분석'))
         
         
-if select_language =='금리와 집값 빠르게 파악하기':  
+if select_language =='금리와 집값의 상관관계':  
     plotting_demo()
   
 
         
-elif select_language =='야구 순위와 승률 빠르게 파악하기':
+elif select_language =='야구 순위와 승률의 상관관계':
     bar_chart()
