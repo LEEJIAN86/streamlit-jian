f682c01a788c4455e02c5eca20692dc1f041d046
LEEJIAN86 <123149534+LEEJIAN86@users.noreply.github.com>
2023-02-01 16:59:47 +0900

    Update jian07.py

--- jian07.py
+++ jian07.py
@@ -6,9 +6,11 @@ import numpy as np
 st.title('Jian\'s :orange[library] :books: graph')
 
 def  plotting_demo():
-    uploaded_file = st.file_uploader("Choose a file")
+    
+    #uploaded_file = st.file_uploader("Choose a file")
 
-    money=pd.read_csv(uploaded_file)
+    #money=pd.read_csv(uploaded_file)
+    money = pd.read_csv("money_data7.csv")
     
     option = st.selectbox(
         'How would you like to choice year ?',
